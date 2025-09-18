c1cecda10f41e5dc6eed5a304ea77d8d7c5d35c7
Jan <jaschubert04@gmail.com>
2025-09-18 10:26:41 +0200

    config for easy custom changes

---
+++ config.js
@@ -0,0 +1,9 @@
+export const config = {
+    starCount: 50,
+    starSpeed: 1.5,
+    starSize: 3,
+    starColor: 'ghostwhite',
+    backgroundColor: '#040720',
+    lineColor: "blue",
+    lineLength: 200
+}
