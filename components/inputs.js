edc10c75b8e61fe8e6e03250e388ed3755f1dd79
Atlas-Zero <jaschubert04@gmail.com>
2026-03-08 21:39:33 +0100

    cleanup

--- components/inputs.js
+++ components/inputs.js
@@ -1,40 +1,30 @@
 const overlay = document.querySelector('#overlay')
 
 export const configLabels = [
-    "starCount",
-    "starSpeed",
-    "starSize",
-    "starColor",
-    "backgroundColor",
-    "uiForegroundColor",
-    "uiBackgroundColor",
-    "lineColor",
-    "lineDistance",
+    'starCount',
+    'starSpeed',
+    'starSize',
+    'starColor',
+    'backgroundColor',
+    'uiForegroundColor',
+    'uiBackgroundColor',
+    'lineColor',
+    'lineDistance',
 ]
 
-
 export function initInputs(config) {
-    overlay.innerHTML = "";
+    overlay.innerHTML = '';
 
     const inputs = [];
 
     for (let i = 0; i < 9; i++) {
-        const input = document.createElement("input");
-        input.dataset.index = i; // store which label it belongs to
+        const input = document.createElement('input');
         input.id = configLabels[i]
-        input.style.position = "absolute";
-        input.style.font = "22px Arial";
-        input.style.background = "transparent";
-        input.style.border = "1px solid" + config.uiBackgroundColor;
-        input.style.width = "150px";
-
-        // add input listener once
-        input.addEventListener('keypress', (e) => {
-            if (e.key == "Enter") {
-                const idx = parseInt(e.target.dataset.index);
-                config[configLabels[idx]] = e.target.value;
-            }
-        });
+        input.style.position = 'absolute';
+        input.style.font = '22px Arial';
+        input.style.background = 'transparent';
+        input.style.border = '1px solid' + config.uiBackgroundColor;
+        input.style.width = '150px';
 
         inputs.push(input);
         overlay.appendChild(input);
